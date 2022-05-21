4aea0f3646104aa5651992a09c29eab745e940c6
cheddar <nowaczewskijack@gmail.com>
2022-05-21 15:26:42 -0400

    add multiple options and questions. replacing text value to null and add new text for new question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 let questionBox = document.querySelector(".questionBox");
 let optionDisplay = document.querySelector(".optionDisplay");
+let questionLiOne = document.createElement("button");
+let questionLiTwo = document.createElement("button");
+let questionLiThree = document.createElement("button");
 
 const test = {
   timer: 0,
   currentQuestion: 0,
+  currentOptions: 0,
   questions: [
     {
       question: "what am the sky",
@@ -12,8 +16,13 @@ const test = {
     },
     {
       question: "Taco Tuesday Menu",
-      answer: "blurple",
-      options: ["plot", "tulip", "frick"],
+      answer: "YAA",
+      options: ["Yeet", "YAA", "SCREE"],
+    },
+    {
+      question: "bow di bow dang diggy diggy to up jump the boogy",
+      answer: "SCREEE",
+      options: ["SCREEE", "bow", "e=mc2"],
     },
   ],
 };
@@ -44,13 +53,12 @@ function start() {
   //how do i iterate over questions
   questionBox.innerHTML = test.questions[test.currentQuestion].question;
   // create a li for answer options.
-  var questionLiOne = document.createElement("button");
-  var questionLiTwo = document.createElement("button");
-  var questionLiThree = document.createElement("button");
+
   // attach class to li
   questionLiOne.classList.add("answerOptions");
   questionLiTwo.classList.add("answerOptions");
   questionLiThree.classList.add("answerOptions");
+
   // attach answer options to ul
   optionDisplay.appendChild(questionLiOne);
   optionDisplay.appendChild(questionLiTwo);
@@ -78,10 +86,10 @@ function start() {
     answerQuestion1(test.questions[test.currentQuestion].options[2]);
   });
 }
-// console.log("answer question One click", test.questions[test.currentQuestion].answer);
+
 function answerQuestion1(answerChoice) {
   // shows time left in console.log
-  console.log(test.timer);
+  console.log("current Time = ", test.timer);
   // validate answer
   //  does answer match option.
   console.log(answerChoice, test.questions[test.currentQuestion].answer);
@@ -89,10 +97,21 @@ function answerQuestion1(answerChoice) {
   if (aq1) {
     console.log("was option 1 right? : ", aq1);
     test.currentQuestion++;
+    // remove older buttons
+    questionLiOne.innerHTML = "";
+    questionLiTwo.innerHTML = "";
+    questionLiThree.innerHTML = "";
+
+    test.currentOptions++;
     console.log("Current Question Iteration: ", test.currentQuestion);
+    showQuestion2();
   } else {
     console.log("False deduction of 10 seconds");
-    test.timer -= 10;
+    let deductedTime = (test.timer -= 10);
+    console.log(
+      "Current Time after Deduction for incorrect answer: ",
+      deductedTime
+    );
   }
   // to next question if other option is selected remove 10 seconds
   // does the answer provide match answer for the question
@@ -100,6 +119,26 @@ function answerQuestion1(answerChoice) {
   // if last question, call end test.
   // else progress next question
   // else no: display wrong message
+}
+function showQuestion2() {
+  // add new question to questionBox
+  questionBox.innerHTML = test.questions[test.currentQuestion].question;
+  // add new buttons
+  optionDisplay.appendChild(questionLiOne);
+  optionDisplay.appendChild(questionLiTwo);
+  optionDisplay.appendChild(questionLiThree);
+
+  // put answers text in li
+  questionLiOne.appendChild(
+    document.createTextNode(test.questions[test.currentQuestion].options[0])
+  );
+
+  questionLiTwo.appendChild(
+    document.createTextNode(test.questions[test.currentQuestion].options[1])
+  );
+  questionLiThree.appendChild(
+    document.createTextNode(test.questions[test.currentQuestion].options[2])
+  );
 }
 
 function endTest() {
