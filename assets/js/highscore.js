ae0766233e39a410be44ee5a402e854ec88bfeef
cheddar <nowaczewskijack@gmail.com>
2022-05-22 16:26:35 -0400

    add score list with values from concatenation

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,3 +1,5 @@
+let highestScore = localStorage.getItem("HighScore");
+
 document.addEventListener("DOMContentLoaded", function gettingHighScores() {
   // from local Storage.
   let highestScore = localStorage.getItem("HighScore");
@@ -5,3 +7,33 @@ document.addEventListener("DOMContentLoaded", function gettingHighScores() {
   let localStorageScore = JSON.stringify(highestScore);
   document.querySelector("span").innerHTML = highestScore;
 });
+
+function addHighScore() {
+  // capture username.
+  let textFieldEntry = document.getElementById("hScore");
+  let userNameInput = textFieldEntry.addEventListener("submit", addHighScore);
+  // use for information display of score and username submission.
+  let myArray = {
+    key: userNameInput,
+    Value: highestScore,
+  };
+  let myArray1 = JSON.stringify(myArray);
+  //   set username to local storage
+  console.log("userNameInput", userNameInput);
+  //   add data to local storage.
+  localStorage.setItem("User Name", myArray1);
+
+  //   select ordered list
+  let scoreBoard = document.querySelector("ol");
+  //create list element
+  let createLi = document.createElement("li");
+  // take data and concatenate a string.
+  let concatenateNameScore = myArray.key + " " + myArray.Value;
+  // append username to ordered list
+  scoreBoard.append(createLi, concatenateNameScore);
+  // use local storage to persist data
+}
+
+// use this nonsense to set to local storage.
+// let highestScore = localStorage.getItem("HighScore");
+// let localStorageScore = JSON.stringify(highestScore);
